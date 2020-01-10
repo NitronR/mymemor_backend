eabcf3a1dfeb1aabede7408a6fb1c2f199e0c41e
rohit-kumar6 <rohit.kumar41996@gmail.com>
2020-01-10 15:37:49 +0530

    Added search based on username and name differentiate by @

--- src/main/java/com/mymemor/mymemor/controller/SearchController.java
+++ src/main/java/com/mymemor/mymemor/controller/SearchController.java
@@ -1,7 +1,6 @@
 package com.mymemor.mymemor.controller;
 
 import com.mymemor.mymemor.exceptions.EmptyQuery;
-import com.mymemor.mymemor.model.Account;
 import com.mymemor.mymemor.model.User;
 import com.mymemor.mymemor.repository.AccountRepository;
 import com.mymemor.mymemor.repository.UserRepository;
@@ -11,6 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,31 +25,38 @@ public class SearchController {
     private UserRepository userRepository;
     @Autowired
     private AccountRepository accountRepository;
+    @PersistenceContext
+    private EntityManager em;
 
-    @GetMapping("/search/{q}")
-    public List getSearchResultViaAjax(@PathVariable(value = "q") String query) throws EmptyQuery {
+    private List<User> getUserById(List o){
         List<User> list_user = new ArrayList<>();
-        List<User> all_user = new ArrayList<>();
-        List<Account> all_account = new ArrayList<>();
-
-        List<String> username = new ArrayList<>();
-
-        if(query.length()==0)
-            throw new EmptyQuery("Query can't be empty");
-
-        if(query.startsWith("@")){
-
-            all_account = accountRepository.findAll();
-            for(Account ac: all_account) {
-                list_user.add(ac.getUser());
-            }
-
-//            Account account = accountRepository.findById((long) 1).orElseThrow();
-//            list_user.add(account.getUser());
-        }else{
-            User user = userRepository.findById((long) 1).orElseThrow();
+        for(Object id: o){
+            User user = userRepository.findById(((BigInteger) id).longValue()).orElseThrow();
             list_user.add(user);
         }
         return list_user;
+    }
+
+    private List getResultBasedOnUsername(EntityManager em, String query){
+        Query q =  em.createNativeQuery("select id from accounts where username LIKE ? ");
+        q.setParameter(1, "%"+query+"%");
+        return q.getResultList();
+    }
+
+    private List getResultBasedOnName(EntityManager em, String query){
+        Query q =  em.createNativeQuery("select id from users where name LIKE ? ");
+        q.setParameter(1, "%"+query+"%");
+        return q.getResultList();
+    }
+
+    @GetMapping("/search/{q}")
+    public List getSearchResultViaAjax(@PathVariable(value = "q") String query) throws EmptyQuery {
+        if(query.length()==0)
+            throw new EmptyQuery("Query can't be empty");
+        if(query.startsWith("@")){
+            return getUserById(getResultBasedOnUsername(em,query.substring(1)));
+        }else{
+            return getUserById(getResultBasedOnName(em,query));
+        }
     }
 }
